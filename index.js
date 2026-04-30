0473ecf54f002edf4c5ff6d49743f88162f0b1eb
fussycats2 <fussycats2@naver.com>
2026-04-30 14:04:44 +0900

    init project

---
+++ index.js
@@ -0,0 +1,13 @@
+let a = 1;
+let b = 2;
+console.log(__dirname);
+
+console.log(a + b);
+
+function sum(a, b) {
+    console.log(a);
+    console.log(b);
+    return a + b;
+}
+const c = sum(a, b);
+console.log(c);
