6c082d18eb72dc71865b0dcf612003b96cccf4d8
fussycats2 <fussycats2@naver.com>
2026-05-07 13:34:00 +0900

    5.7

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
 // express의 부트스트랩 파일
+import path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
 import userRoute from "./routes/user-route.js";
+import postRoute from "./routes/post-route.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.json());
 
-app.use("/users", userRoute);
-
-app.get("/", (req, res) => {
+app.use((req, res, next) => {
+  // 로깅
   const reqUrl = req.url;
   const reqMethod = req.method;
   console.log(reqUrl, reqMethod);
+  next();
+});
+
+app.use((req, res, next) => {
+  // 인증 확인
+  const auth = req.headers.authorization;
+  console.log(auth);
+  next();
+});
+
+app.get("/", (req, res) => {
   res.status(202).send("Hello World");
 });
 
-app.get("/posts", (req, res) => {
-  const posts = [
-    { id: 1, title: "Hello World" },
-    { id: 2, title: "Hello World" },
-    { id: 3, title: "Hello World" },
-    { id: 4, title: "Hello World" },
-  ];
-  res.json(posts);
+app.use("/users", userRoute);
+app.use("/posts", postRoute);
+
+app.use((req, res) => {
+  console.log("Not Found");
+  res.status(404).render("404", {
+    title: "페이지를 찾을 수 없습니다",
+    path: req.originalUrl,
+  });
 });
 
 app.listen(PORT, () => {
